c0002228c638fc7dcfe4ae96493e784e10d59604
Yuichi Namiki <namiki081025@gmail.com>
2021-07-20 17:20:24 +0900

    php_task10

--- task.php
+++ task.php
@@ -115,14 +115,32 @@ foreach($names as $key => $name){
 }
 print_r($member_names);
 echo PHP_EOL;
-?>
+
 print("#####q10#####".PHP_EOL);
 $foods = ["いか", "たこ", "うに", "しゃけ", "うにぎり", "うに軍艦", "うに丼", "高級なうに"];
 
   # 以下に回答を記載
+foreach($foods as $food) {
+  if(preg_match('/うに/',$food)){
+    print('好物です'.PHP_EOL);
+  }else{
+    print('まぁまぁ好きです'.PHP_EOL);
+  }
+}
 
 echo PHP_EOL;
 
+foreach($foods as $food) {
+  if(preg_match('/うに/',$food)){
+    print($food.'は、好物です'.PHP_EOL);
+  }else{
+    print($food.'は、まぁまぁ好きです'.PHP_EOL);
+  }
+}
+
+
+echo PHP_EOL;
+?>
 print("#####q11#####".PHP_EOL);
 $sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
 
